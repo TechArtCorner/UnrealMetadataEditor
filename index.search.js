fdbeb062d4a5053982e6b0d7394255bda244f0e9
TechArtCorner <techartcorner@gmail.com>
2025-11-18 21:45:23 +0100

    Added Gumroad URL

--- index.search.js
+++ index.search.js
@@ -41,8 +41,8 @@ var relearn_search_index = [
   },
   {
     "breadcrumb": "",
-    "content": "Unreal Metadata Tags Editor A friendly user interface for editing Unreal asset metadata tags. Apply changes to one or more assets at once.\nGet it on Fab! ",
-    "description": "Unreal Metadata Tags Editor A friendly user interface for editing Unreal asset metadata tags. Apply changes to one or more assets at once.\nGet it on Fab! ",
+    "content": "Unreal Metadata Tags Editor A friendly user interface for editing Unreal asset metadata tags. Apply changes to one or more assets at once.\nGet it on Fab! or on Gumroad! ",
+    "description": "Unreal Metadata Tags Editor A friendly user interface for editing Unreal asset metadata tags. Apply changes to one or more assets at once.\nGet it on Fab! or on Gumroad! ",
     "tags": [],
     "title": "Overview",
     "uri": "/index.html"
